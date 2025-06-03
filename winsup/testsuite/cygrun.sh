b6d6f37010ef83c389c7d71b30cd66ef282124dc
Radek Bartoň <radek.barton@microsoft.com>
2025-06-03 13:53:04 +0200

    SKIP: Cygwin: CI: build AArch64 Cygwin using Ubuntu cross-toolchain

--- winsup/testsuite/cygrun.sh
+++ winsup/testsuite/cygrun.sh
@@ -13,5 +13,10 @@ then
     windows_runtime_root=$(cygpath -m $runtime_root)
     $mingwtestdir/cygrun "$exe -v -cygwin $windows_runtime_root/cygwin1.dll"
 else
-    cygdrop $mingwtestdir/cygrun $exe
+    if uname | grep -qi cygwin; then
+        cygdrop $mingwtestdir/cygrun $exe
+    else
+        # Running under WSL.
+        timeout --preserve-status 300 "$exe"
+    fi
 fi
